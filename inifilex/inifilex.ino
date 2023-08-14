69ec8a56e138f4dfb5b883f68620244b18af890b
stephcache <70350966+stephcache@users.noreply.github.com>
2023-08-14 21:55:45 +0200

    Add files via upload

--- inifilex/inifilex.ino
+++ inifilex/inifilex.ino
@@ -8,6 +8,12 @@ void setup() {
   while (!Serial);
   delay(1000);
 
+  if (!SD.begin(configFile.getSDPin()))
+  {
+    Serial.println("Carte SD non détectée.");
+    while (1);  // Arrêtez l'exécution en boucle si la carte SD n'est pas détectée
+  }
+
   String motDePasse = configFile.readVariable("wifi", "mdp");
   Serial.println("Mot de passe WiFi : " + motDePasse);
 
